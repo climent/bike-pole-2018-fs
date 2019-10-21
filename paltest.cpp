2da433da3e7011701d5ba88b6decddcf17f1f992
Jesus Climent <climent@jesus-mac-mini.lan>
2019-10-21 11:56:20 -0400

    Constrain index in palette to the number of LEDs in the string.

--- paltest.cpp
+++ paltest.cpp
@@ -7,8 +7,8 @@ void PalTest::Render() {
   fadeToBlackBy(dst, NUM_LEDS, 1);
   for (uint16_t j = 0; j < NUM_LEDS; j++) {
     int palindex = j;
-    if (NUM_LEDS < 255) map(palindex, 0, NUM_LEDS, 0, 255);
-    CRGB c = finalPalette[0][palindex];
+    palindex = map(palindex, 0, NUM_LEDS, 0, 255);
+    CRGB c = finalPalette[pal][palindex];
     dst[SetPixelsSingle(j)] = c;
   }
 }
